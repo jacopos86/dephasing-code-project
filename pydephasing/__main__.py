60a14619d33eb676cdbcc063f73155ea958529a2
Jacopo <simonij@tcd.ie>
2022-05-24 10:47:24 -0700

    input_create added

--- pydephasing/__main__.py
+++ pydephasing/__main__.py
@@ -1,3 +1,36 @@
 import sys
-# 
-print("sei una schiappa")
+from pydephasing.create_input_files import gen_input_files
+#
+print("-------------------------------------------") 
+print("-------- START PYDEPHASING PROGRAM --------")
+print("-------------------------------------------")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#
+calc_type = sys.argv[1]
+if calc_type == "--energy":
+    print("-------------------------------------------")
+    print("--- ENERGY LEVELS DEPHASING CALCULATION ---")
+    # prepare energy dephasing calculation
+elif calc_type == "--spin":
+    print("-------------------------------------------")
+    print("------- SPIN DEPHASING CALCULATION --------")
+    # prepare spin dephasing calculation
+    calc_type2 = sys.argv[2]
+    if calc_type2 == "--homo":
+        print("-------------------------------------------")
+        print("-------- HOMOGENEOUS CALCULATION ----------")
+        print("------------ COMPUTING T2 -----------------")
+        print("-------------------------------------------")
+        gen_input_files()
+    elif calc_type2 == "--inhomo":
+        print("-------------------------------------------")
+        print("------- INHOMOGENEOUS CALCULATION ---------")
+        print("------------ COMPUTING T2* ----------------")
+        print("-------------------------------------------")
+        
+else:
+    print("-------------------------------------------")
+    print("------ CALC_TYPE FLAG NOT RECOGNIZED ------")
+    print("---------- END OF THE PROGRAM -------------")
+    print("-------------------------------------------")
+    sys.exit(1)
---
+++ pydephasing/create_input_files.py
@@ -0,0 +1,4 @@
+# this subroutine creates the input files
+# to be run in the VASP calculation
+def gen_input_files():
+    return 0
